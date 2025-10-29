c1aa7936a74eef1571fd0eb5f9795838df684c43
Joshan Kana <joshankana@gmail.com>
2025-10-29 23:29:02 +0800

    added required tools to flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "flake for hotkey-visualiser";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        packages = with pkgs; [
+          nixfmt-tree
+          qmk
+          keymap-drawer
+        ];
+
+        # Create shell scripts
+        scripts = [
+          (pkgs.writeScriptBin "rust-init" ''
+            ./scripts/keymap.sh
+          '')
+          (pkgs.writeScriptBin "another-script" ''
+            ./scripts/another-script.sh
+          '')
+        ];
+      in
+        {
+          # For nix develop
+          devShells = {
+            default = pkgs.mkShell {
+              packages = packages ++ scripts;
+            };
+          };
+
+          # For nix shell
+          packages = {
+            default = pkgs.buildEnv {
+              name = "hotkey-visualiser";
+              paths = packages ++ scripts;
+            };
+          };
+        });
+}
